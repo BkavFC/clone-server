1c77a5a0035e9916942678e4711a6e59a0bf9929
Do Tran <38042898+BkavFC@users.noreply.github.com>
2024-05-16 17:59:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,62 +50,97 @@ done
 mysql_user="${domain%%.*}"
 mysql_pass=`openssl rand -base64 12`
 mysql_database="${domain%%.*}"
+api_storage="https://storage.job3s.vn"
+read -p "Nhap Access Key duoc cung cap: " access_key
+read -p "Nhap Secrect Key duoc cung cap: " secrect_key
+echo -e "\033[1;33mDang kiem tra Key...\033[0m"
+#access_key="2jic3LO7nv1D8ijaOZWQ"
+#secrect_key="ubuHqhZkTwLvux8Tqp2qffT6WOQxLUONIcGTOEdK"
+wget -q https://dl.min.io/client/mc/release/linux-amd64/mc -O /usr/local/bin/mc -o /dev/null
+chmod +x /usr/local/bin/mc
+mc alias set minio $api_storage $access_key $secrect_key >> /dev/null
+if mc ls minio/source &> /dev/null; then
+    echo -e "\033[0;32mAccess Key va Secret Key hop le.\033[0m"
+    echo "Bat dau qua trinh cai dat........................"
+else
+    echo -e "\033[0;31mAccess Key va Secret Key khong dung. Vui long nhap lai\033[0m"
+    exit 1
+fi
 #2.setting enviroment
-#2.1.general
 timedatectl set-timezone Asia/Ho_Chi_Minh
-apt install zip unzip vim net-tools git curl -y
+apt-get install zip unzip vim net-tools git expect curl -y >> /dev/null
 curl -Lso- https://raw.githubusercontent.com/nhanhoadocs/ghichep-cmdlog/master/cmdlog.sh | bash
-git clone https://github.com/BkavFC/clone-server.git
-ufw allow 80
-ufw allow 443
-ufw allow from $trusted_ip proto tcp to any port 22
-ufw allow from $trusted_ip proto tcp to any port 3306
-ufw enable -y
+ufw allow 80 >> /dev/null
+ufw allow 443 >> /dev/null
+ufw allow 22 >> /dev/null
+expect -c 'set timeout 2; spawn ufw enable; expect "Command may disrupt existing ssh connections. Proceed with operation (y|n)?"; send "y\n"; interact'
 systemctl restart ufw
 #2.1.install redis
-apt install redis-server -y
-systemcrl enable redis-server
+apt-get install redis-server -y >> /dev/null
+systemctl enable redis-server
 systemctl restart redis-server
+pid_redis=`systemctl status redis-server | grep "Main PID" | awk '{print $3}'`
+echo -e "\033[0;32m(1/4) Cai dat thanh cong Redis. PID running: $pid_redis\033[0m"
 #2.1.install php
-apt install software-properties-common
-add-apt-repository ppa:ondrej/php
-apt install php$version_php-cli php$version_php-fpm php$version_php-mbstring php$version_php-zip php$version_php-xml php$version_php-curl php$version_php-gd php$version_php-mysql php$version_php-redis -y
+apt-get install software-properties-common -y >> /dev/null
+expect -c 'set timeout 2; spawn add-apt-repository ppa:ondrej/php; expect "Press \[ENTER\] to continue or Ctrl-c to cancel adding it."; send "\r"; interact' >> /dev/null
+apt-get install php$version_php-cli php$version_php-fpm php$version_php-mbstring php$version_php-zip php$version_php-xml php$version_php-curl php$version_php-gd php$version_php-mysql php$version_php-redis -y >> /dev/null
 systemctl enable php$version_php-fpm
 systemctl restart php$version_php-fpm
+pid_php_fpm=`systemctl status php$version_php-fpm | grep "Main PID" | awk '{print $3}'`
+echo -e "\033[0;32m(2/4) Cai dat thanh cong php$version_php-fpm. PID running: $pid_php_fpm\033[0m"
 #2.2.install mysql
-apt install mysql-server -y
-mysql_secure_installation
-###config manual
-mv /etc/mysql/mysqld.conf.d/mysqld.conf /etc/mysql/mysqld.conf.d/mysqld.conf.ori
-cp /root/mysqld.conf /etc/mysql/mysqld.conf.d/
-systemctl enable mysql-server
-systemctl restart mysql-server
+#apt-get install mysql-server -y >> /dev/null
+#expect auto_fill_mysql_config.sh
+#mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.ori
+#mv /root/mysqld.cnf /etc/mysql/mysql.conf.d/
+#systemctl enable mysql
+#systemctl restart mysql
+#pid_mysql_server=`systemctl status mysql | grep "Main PID" | awk '{print $3}'`
+#echo -e "\033[0;32m(3/6) Cai dat thanh cong mysql-8. PID running: $pid_mysql_server\033[0m"
 #2.3.install nginx
-apt-get update && apt-get upgrade -y
+apt-get update >> /dev/null
+apt-get upgrade -y  >> /dev/null
 apt-get install nginx -y
 systemctl enable nginx
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.ori
-cp /root/clone-server/nginx.conf /etc/nginx/nginx.conf
-cp /root/clone-server/domain.conf /etc/nginx/conf.d/$domain.conf
+mc cp minio/config/nginx.conf /etc/nginx/nginx.conf
 mkdir -p /etc/nginx/ssl_cert/$domain/
-cp /root/$domain.* /etc/nginx/ssl_cert/$domain/
-#3.update data
-#3.1.restore database
-unzip *.zip
-mysql -u "$mysql_user" --password="$mysql_pass" -e "create database `$mysql_database`"
-mysql -u "$mysql_user" --password="$mysql_pass" "$mysql_database" < /root/*.sql
+mc cp minio/config/domain.conf /etc/nginx/conf.d/domain.conf
+sed -i "s/domain/$domain/g" /etc/nginx/conf.d/domain.conf
+sed -i "s/version_php/$version_php/g" domain.conf
+mv /root/domain.conf /etc/nginx/conf.d/$domain.conf
+#cp /root/$domain.* /etc/nginx/ssl_cert/$domain/
 #3.2.unzip source code
-mv domain /var/www/html/
-sed -i "s/domain/$domain/g" /root/.env
-sed -i "s/mysql_user/$mysql_user/g" /root/.env
-sed -i "s/mysql_pass/$mysql_pass/g" /root/.env
-sed -i "s/mysql_database/$mysql_database/g" /root/.env
-cp /root/.env /var/www/html/$domain/
+echo -e "\033[0;32m(3/4) Updating data...............\033[0m"
+mkdir -p /var/www/html/
+mc cp minio/source/source.zip /var/www/html/
+cd /var/www/html && unzip source.zip >> /dev/null
+cd /var/www/html && rm -rf source.zip
+cd /var/www/html && mv source $domain
+cd /var/www/html/$doamin && sed -i "s/domain/$domain/g" /tmp/.env
+cd /var/www/html/$doamin && sed -i "s/mysql_user/$mysql_user/g" /tmp/.env
+cd /var/www/html/$doamin && sed -i "s/mysql_pass/$mysql_pass/g" /tmp/.env
+cd /var/www/html/$doamin && sed -i "s/mysql_database/$mysql_database/g" /tmp/.env
+rm -rf /var/www/html/$domain/.env && mv /tmp/.env /var/www/html/$domain/
 cd /var/www/html/$domain/ && rm -rf bootstrap/cache
 cd /var/www/html/$domain/ && mkdir -p bootstrap/cache
 cd /var/www/html/$domain/ && chmod -R 777 bootstrap/cache
+cd /var/www/html/$domain/ && chmod -R 777 strorage
 cd /var/www/html/$domain/ && php artisan cache:clear
-cd /var/www/html/$domain/ && php artisan wn:sitemap:run-all
 cd /var/www/html/$domain/ && php artisan optimize
 cd /var/www/html/$domain/ && php artisan route:clear
 systemctl restart nginx
+pid_nginx=`systemctl status nginx | grep "Main PID" | awk '{print $3}'`
+echo -e "\033[0;32m(3/4) Cai dat thanh cong nginx. PID running: $pid_nginx\033[0m"
+ip_public=`curl -s ifconfig.me`
+date1=$(date '+%Y%m%d_%H%M')
+echo -e "\033[0;32m(4/4) Update data thanh cong\033[0m"
+touch /tmp/$ip_public_$date1.log
+echo "Thong tin cai dat domain $domain" >> /tmp/$ip_public_$date1.log
+echo "IP Public = $ip_public" >> /tmp/$ip_public_$date1.log
+echo "Database Name = $mysql_database" >> /tmp/$ip_public_$date1.log
+echo "Database User = $mysql_user" >> /tmp/$ip_public_$date1.log
+echo "Database Pass = $mysql_pass" >> /tmp/$ip_public_$date1.log
+mc cp /tmp/$ip_public_$date1.log minio/logs/
+#web_status=`curl -o /dev/null -s -w '%{http_code}' https://$domain`
