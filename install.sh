283aae4b3eb477c56d1850870091629352908608
Do Tran <38042898+BkavFC@users.noreply.github.com>
2024-05-17 16:46:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,14 +45,16 @@ while true; do
         ;;
 esac
 done
-mysql_user="${domain%%.*}"
+date0=$(date '+%Y%m%d_%H%M')
+mysql_user="${domain%%.*}_$date0"
 mysql_pass=`openssl rand -base64 8`
-mysql_pass_root=`openssl rand -base64 16`
-mysql_database="${domain%%.*}"
+mysql_database="${domain%%.*}_$date0"
 api_storage="https://storage.job3s.vn"
 read -p "Nhap Access Key duoc cung cap: " access_key
 read -p "Nhap Secrect Key duoc cung cap: " secrect_key
 echo -e "\033[1;33mDang kiem tra Key...\033[0m"
+#access_key="2jic3LO7nv1D8ijaOZWQ"
+#secrect_key="ubuHqhZkTwLvux8Tqp2qffT6WOQxLUONIcGTOEdK"
 wget -q https://dl.min.io/client/mc/release/linux-amd64/mc -O /usr/local/bin/mc -o /dev/null
 chmod +x /usr/local/bin/mc
 mc alias set minio $api_storage $access_key $secrect_key >> /dev/null
@@ -65,7 +67,7 @@ else
 fi
 #2.setting enviroment
 timedatectl set-timezone Asia/Ho_Chi_Minh
-apt-get install zip unzip vim net-tools git expect curl -y >> /dev/null
+apt-get install zip unzip vim net-tools git expect mysql-client curl -y >> /dev/null
 curl -Lso- https://raw.githubusercontent.com/nhanhoadocs/ghichep-cmdlog/master/cmdlog.sh | bash >> /dev/null 
 ufw allow 80 >> /dev/null
 ufw allow 443 >> /dev/null
@@ -77,7 +79,7 @@ apt-get install redis-server -y >> /dev/null
 systemctl enable redis-server >> /dev/null
 systemctl restart redis-server
 pid_redis=`systemctl status redis-server | grep "Main PID" | awk '{print $3}'`
-echo -e "\033[0;32m(1/6) Cai dat thanh cong Redis. PID running: $pid_redis\033[0m"
+echo -e "\033[0;32m(1/5) Cai dat thanh cong Redis. PID running: $pid_redis\033[0m"
 #2.1.install php
 apt-get install software-properties-common -y >> /dev/null
 expect -c 'set timeout 2; spawn add-apt-repository ppa:ondrej/php; expect "Press \[ENTER\] to continue or Ctrl-c to cancel adding it."; send "\r"; interact' >> /dev/null
@@ -85,36 +87,32 @@ apt-get install php$version_php-cli php$version_php-fpm php$version_php-mbstring
 systemctl enable php$version_php-fpm >> /dev/null
 systemctl restart php$version_php-fpm
 pid_php_fpm=`systemctl status php$version_php-fpm | grep "Main PID" | awk '{print $3}'`
-echo -e "\033[0;32m(2/6) Cai dat thanh cong php$version_php-fpm. PID running: $pid_php_fpm\033[0m"
+echo -e "\033[0;32m(2/5) Cai dat thanh cong php$version_php-fpm. PID running: $pid_php_fpm\033[0m"
 #2.2.install mysql
-apt-get install mysql-server -y >> /dev/null
-expect -c  'set timeout 2;
-            spawn mysql_secure_installation;
-            expect "Press y|Y for Yes, any other key for No:";
-            send "n\n";
-            expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :";
-            send "y\n";
-            expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :";
-            send "n\n";
-            expect "Remove test database and accescs to it? (Press y|Y for Yes, any other key for No) :";
-            send "y\n";
-            expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :";
-            send "y\n";
-            interact' >> /dev/null
-mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.ori
-mc cp minio/config/mysqld.cnf /etc/mysql/mysql.conf.d/
-mysql -u root --password="" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysql_pass_root'"
-mysql -u root --password="$mysql_pass_root" -e "CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$mysql_pass_root'"
-mysql -u root --password="$mysql_pass_root" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'"
-mysql -u root --password="$mysql_pass_root" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
-mysql -u root --password="$mysql_pass_root" -e "create database $mysql_database"
-mysql -u root --password="$mysql_pass_root" -e "CREATE USER '$mysql_user'@'%' IDENTIFIED WITH mysql_native_password BY '$mysql_pass'"
-mysql -u root --password="$mysql_pass_root" -e "GRANT SELECT, INSERT, UPDATE, DELETE ON $mysql_database.* TO '$mysql_user'@'%'"
-mysql -u root --password="$mysql_pass_root" -e "FLUSH PRIVILEGES"
-systemctl enable mysql >> /dev/null
-systemctl restart mysql
-pid_mysql_server=`systemctl status mysql | grep "Main PID" | awk '{print $3}'`
-echo -e "\033[0;32m(3/6) Cai dat thanh cong mysql-8. PID running: $pid_mysql_server\033[0m"
+#apt-get install mysql-server -y >> /dev/null
+#expect -c  'set timeout 2;
+#            spawn mysql_secure_installation;
+#            expect "Press y|Y for Yes, any other key for No:";
+#            send "n\n";
+#            expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :";
+#            send "y\n";
+#            expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :";
+#            send "n\n";
+#            expect "Remove test database and accescs to it? (Press y|Y for Yes, any other key for No) :";
+#            send "y\n";
+#            expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :";
+#            send "y\n";
+#            interact' >> /dev/null
+#mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.ori
+#mc cp minio/config/mysqld.cnf /etc/mysql/mysql.conf.d/
+#mysql -u root --password="" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysql_pass_blind'"
+#mysql -u root --password="$mysql_pass_blind" -e "CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$mysql_pass_blind'"
+#mysql -u root --password="$mysql_pass_blind" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'"
+#mysql -u root --password="$mysql_pass_blind" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
+#systemctl enable mysql >> /dev/null
+#systemctl restart mysql
+#pid_mysql_server=`systemctl status mysql | grep "Main PID" | awk '{print $3}'`
+#echo -e "\033[0;32m(3/6) Cai dat thanh cong mysql-8. PID running: $pid_mysql_server\033[0m"
 #2.3.install nginx
 apt-get update >> /dev/null
 apt-get upgrade -y  >> /dev/null
@@ -128,7 +126,7 @@ sed -i "s/version_php/$version_php/g" /etc/nginx/conf.d/domain.conf
 mv /etc/nginx/conf.d/domain.conf /etc/nginx/conf.d/$domain.conf
 systemctl restart nginx
 pid_nginx=`systemctl status nginx | grep "Main PID" | awk '{print $3}'`
-echo -e "\033[0;32m(4/6) Cai dat thanh cong nginx. PID running: $pid_nginx\033[0m"
+echo -e "\033[0;32m(3/5) Cai dat thanh cong nginx. PID running: $pid_nginx\033[0m"
 apt-get install certbot python3-certbot-nginx -y >> /dev/null
 expect -c  'set timeout 5;
             spawn certbot --nginx -d '$domain' -d 'www.$domain';
@@ -145,10 +143,22 @@ systemctl reload nginx
 #3.1.restore database
 mc cp minio/source/sql.zip /tmp/
 cd /tmp/ && unzip sql.zip >> /dev/null
-mysql -u root --password="$mysql_pass_root" "$mysql_database" < /tmp/*.sql
+mc cp minio/config/info /tmp/info
+mysql_user_blind=`awk '{print $1}' /tmp/info`
+mysql_pass_blind=`awk '{print $2}' /tmp/info`
+mysql_ip_blind=`awk '{print $3}' /tmp/info`
+mysql_port_blind=`awk '{print $4}' /tmp/info`
+rm -rf /tmp/info
+mysql -u $mysql_user_blind --password="$mysql_pass_blind" -h $mysql_ip_blind -P $mysql_port_blind -e "create database $mysql_database"
+mysql -u $mysql_user_blind --password="$mysql_pass_blind" -h $mysql_ip_blind -P $mysql_port_blind -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
+mysql -u $mysql_user_blind --password="$mysql_pass_blind" -h $mysql_ip_blind -P $mysql_port_blind -e "FLUSH PRIVILEGES"
+mysql -u $mysql_user_blind --password="$mysql_pass_blind" -h $mysql_ip_blind -P $mysql_port_blind "$mysql_database" < /tmp/*.sql
+mysql -u $mysql_user_blind --password="$mysql_pass_blind" -h $mysql_ip_blind -P $mysql_port_blind -e "CREATE USER '$mysql_user'@'%' IDENTIFIED WITH mysql_native_password BY '$mysql_pass'"
+mysql -u $mysql_user_blind --password="$mysql_pass_blind" -h $mysql_ip_blind -P $mysql_port_blind -e "GRANT SELECT, INSERT, UPDATE, DELETE ON $mysql_database.* TO '$mysql_user'@'%'"
+mysql -u $mysql_user_blind --password="$mysql_pass_blind" -h $mysql_ip_blind -P $mysql_port_blind -e "FLUSH PRIVILEGES"
 rm -rf /tmp/sql.zip && rm -rf /tmp/*.sql
 #3.2.unzip source code
-echo -e "\033[0;32m(5/6) Dang cap nhat du lieu...............\033[0m"
+echo -e "\033[0;32m(4/5) Dang cap nhat du lieu...............\033[0m"
 mkdir -p /var/www/html/
 mc cp minio/source/source.zip /var/www/html/
 cd /var/www/html && unzip source.zip >> /dev/null
@@ -168,16 +178,15 @@ cd /var/www/html/$domain/ && php artisan cache:clear
 cd /var/www/html/$domain/ && php artisan optimize
 cd /var/www/html/$domain/ && php artisan route:clear
 systemctl reload nginx
-echo -e "\033[0;32m(6/6) Cap nhat du lieu thanh cong\033[0m"
+echo -e "\033[0;32m(5/5) Cap nhat du lieu thanh cong\033[0m"
 date1=$(date '+%Y%m%d_%H%M')
 touch /tmp/"$domain"_"$date1".log
 ip_public=`curl -s ifconfig.me`
-echo "Thong tin cai dat domain $domain" >> /tmp/"$domain"_"$date1".log
-echo "IP Public = $ip_public" >> /tmp/"$domain"_"$date1".log
-echo "Database Name = $mysql_database" >> /tmp/"$domain"_"$date1".log
-echo "Database User Root = $mysql_pass_root" >> /tmp/"$domain"_"$date1".log
-echo "Database User = $mysql_user" >> /tmp/"$domain"_"$date1".log
-echo "Database Pass = $mysql_pass" >> /tmp/"$domain"_"$date1".log
+echo "Thong tin cai dat domain $domain\n" >> /tmp/"$domain"_"$date1".log
+echo "IP Public = $ip_public\n" >> /tmp/"$domain"_"$date1".log
+echo "Database Name = $mysql_database\n" >> /tmp/"$domain"_"$date1".log
+echo "Database User = $mysql_user\n" >> /tmp/"$domain"_"$date1".log
+echo "Database Pass = $mysql_pass\n" >> /tmp/"$domain"_"$date1".log
 mc cp /tmp/"$domain"_"$date1".log minio/logs/
 rm -rf /tmp/"$domain"_"$date1".log
 rm -rf /var/www/html/$domain/.env
